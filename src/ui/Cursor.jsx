308457089b6520c4a2f7c34ca0b18b3959feefde
shvgpt-070208 <shvgpt070208@gmail.com>
2025-07-05 21:19:14 +0530

    deployement commit

--- src/ui/Cursor.jsx
+++ src/ui/Cursor.jsx
@@ -3,17 +3,17 @@ import { motion } from 'motion/react'
 import React, { useEffect, useState } from 'react'
 
 const Cursor = () => {
-    const [taped,settaped] = useState(false)
+   
     const [mouseposition,setmouseposition] = useState({x:-10,y:-10})
    
     useEffect(()=>{
         window.addEventListener('mousemove',(e)=>setmouseposition({x:e.clientX,y:e.clientY}))
-        window.addEventListener('mousedown',()=>{settaped((prev)=>( !prev));console.log(taped);
-        })
+       
+       
     },[])
     return (
     <motion.div  
- animate={{top:mouseposition.y,left:mouseposition.x,scale:taped?5:1}} transition={{type:'spring',stiffness:100}} className='cursorspan ' />
+ animate={{top:mouseposition.y,left:mouseposition.x}} transition={{type:'spring',stiffness:100}} className='cursorspan ' />
       
 
   )
